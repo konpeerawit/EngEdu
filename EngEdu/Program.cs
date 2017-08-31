779b10151565ea117b208b01f23cc7eb423e43c9
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-08-31 14:25:28 +0700

    5(Computer i start HTML.)

--- EngEdu/Program.cs
+++ EngEdu/Program.cs
@@ -14,6 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2560 semester 1,  I Start my programmer life.");
             Console.WriteLine("Year 2560 semester 2, I learn database ");
+            Console.WriteLine("Year 2561 semester 1,  I Start learn HTML.");
             
 
 
